1bddf2a821c3fd0e7dbd53d5a0c7e62cbfd3be34
erickggarcia <erickgegarcia@gmail.com>
2024-05-13 23:10:08 -0300

    [feat] criação do hook useSummary e estilização dos formulários

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./themes/defaultTheme"
 import { GlobalStyle } from "./styles/global"
 import { Transactions } from "./pages/Transactions"
+import { TransactionsContextProvider } from "./contexts/TransactionsContext"
 
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Transactions />
+      <TransactionsContextProvider>
+        <GlobalStyle />
+        <Transactions />
+      </TransactionsContextProvider>
     </ThemeProvider>
   )
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,30 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export function NewTransactionModal() {
+
+    const newTransactionFormSchema = z.object({
+        description: z.string(),
+        price: z.number(),
+        category: z.string(),
+        // type: z.enum(['income', 'outcome'])
+    })
+
+    type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
+
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<newTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema)
+    })
+
+    async function handleCreateNewTransaction(data: newTransactionFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
     return (
         <Dialog.Portal>
             <Overlay />
@@ -12,11 +34,23 @@ export function NewTransactionModal() {
                     <X size={24} />
                 </CloseButton>
 
-                <form action="">
-                    <input type="text" placeholder='Descrição' required />
-                    <input type="number" placeholder='Preço' />
-                    <input type="text" placeholder='Categoria' />
-                    <button type='submit'>Cadastrar</button>
+                <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input type="text"
+                        placeholder='Descrição'
+                        {...register('description')}
+                        required />
+                    <input type="number"
+                        placeholder='Preço'
+                        {...register('price', { valueAsNumber: true })}
+                    />
+                    <input type="text"
+                        placeholder='Categoria'
+                        {...register('category')}
+                    />
+                    <button
+                        type='submit'
+                        disabled={isSubmitting}
+                    >Cadastrar</button>
                 </form>
 
                 <TransactionType>
